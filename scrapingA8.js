1afd278d466c76b6fb92c7584e46f108720228a6
jirouemontadaaki <mahu_berehat@yahoo.co.jp>
2013-12-09 00:10:58 +0900

    ループ成功

--- scrapingA8.js
+++ scrapingA8.js
@@ -1,6 +1,6 @@
 // ログインIDとパスワード
-var loginId = 'あなたのid';
-var loginPSWD = 'あなたのパスワード';
+var loginId = 'あなたのログインID';
+var loginPSWD = 'あなたのログインパスワード';
 //var kaiinId = '会員ID'; // AsIDいわゆる会員ID、多分つかわない
 var targetUrl='http://www.a8.net/';
 //var targetUrl='file:///ローカルに/保存した/ファイル.html';
@@ -67,118 +67,27 @@ casper.then(function() {
 }).wait(10000);
 
 casper.then(function() {
-	this.click('input#ctck010');
-}).wait(500);
+        var totalCats = 37;
+        for(var i=1; i<= totalCats; i++){
+                this.wait(500, (function(cnt){ 
+			// ここの'(function(cnt){'から下記'})(i))'までが即時実行関数とかいうものらしい。
+			// 英語ではImmediately-Invoked Function Expression、略してIIFE。
+                        var numStr = '0'+String(cnt)+'0';
+                        var inputId = 'input#ctck'+numStr.substring(numStr.length-3, numStr.length);
+                        return function(){
+                                this.click(inputId);
+                                }
+                })(i));
+        }
+});
+// ループ成功
+
 casper.then(function() {
-	this.click('input#ctck020');
-}).wait(500);
-casper.then(function() {
-	this.click('input#ctck030');
-}).wait(500);
-casper.then(function() {
-	this.click('input#ctck040');
-}).wait(500);
-casper.then(function() {
-	this.click('input#ctck050');
-}).wait(500);
-casper.then(function() {
-	this.click('input#ctck060');
-}).wait(500);
-casper.then(function() {
-	this.click('input#ctck070');
-}).wait(500);
-casper.then(function() {
-	this.click('input#ctck080');
-}).wait(500);
-casper.then(function() {
-	this.click('input#ctck090');
-}).wait(500);
-casper.then(function() {
-	this.click('input#ctck100');
-}).wait(500);
-casper.then(function() {
-	this.click('input#ctck110');
-}).wait(500);
-casper.then(function() {
-	this.click('input#ctck120');
-}).wait(500);
-casper.then(function() {
-	this.click('input#ctck130');
-}).wait(500);
-casper.then(function() {
-	this.click('input#ctck140');
-}).wait(500);
-casper.then(function() {
-	this.click('input#ctck150');
-}).wait(500);
-casper.then(function() {
-	this.click('input#ctck160');
-}).wait(500);
-casper.then(function() {
-	this.click('input#ctck170');
-}).wait(500);
-casper.then(function() {
-	this.click('input#ctck180');
-}).wait(500);
-casper.then(function() {
-	this.click('input#ctck190');
-}).wait(500);
-casper.then(function() {
-	this.click('input#ctck200');
-}).wait(500);
-casper.then(function() {
-	this.click('input#ctck210');
-}).wait(500);
-casper.then(function() {
-	this.click('input#ctck220');
-}).wait(500);
-casper.then(function() {
-	this.click('input#ctck230');
-}).wait(500);
-casper.then(function() {
-	this.click('input#ctck240');
-}).wait(500);
-casper.then(function() {
-	this.click('input#ctck250');
-}).wait(500);
-casper.then(function() {
-	this.click('input#ctck260');
-}).wait(500);
-casper.then(function() {
-	this.click('input#ctck270');
-}).wait(500);
-casper.then(function() {
-	this.click('input#ctck280');
-}).wait(500);
-casper.then(function() {
-	this.click('input#ctck290');
-}).wait(500);
-casper.then(function() {
-	this.click('input#ctck300');
-}).wait(500);
-casper.then(function() {
-	this.click('input#ctck310');
-}).wait(500);
-casper.then(function() {
-	this.click('input#ctck320');
-}).wait(500);
-casper.then(function() {
-	this.click('input#ctck330');
-}).wait(500);
-casper.then(function() {
-	this.click('input#ctck340');
-}).wait(500);
-casper.then(function() {
-	this.click('input#ctck350');
-}).wait(500);
-casper.then(function() {
-	this.click('input#ctck360');
-}).wait(500);
-casper.then(function() {
-	this.click('input#ctck370');
-}).wait(500);
-// すべてのカテゴリーにチェック 
-// かなりダサい。ループでなんとかならないものか。
+	this.capture('program_categories_all_clicked.png' , {
+		top: 0, left: 0,
+		width: viewX, height: viewY
+	});
+});
 
 casper.then(function() {
 	this.mouseEvent('click' , 'table#searchTable td.searchBtn a:nth-child(1)');
