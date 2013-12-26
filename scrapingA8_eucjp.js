f2ba6179fd6912f0f39d4c1a7cf0a321f6175a99
jirouemontadaaki <mahu_berehat@yahoo.co.jp>
2013-12-26 10:37:28 +0900

    全プログラム（現在４６８６件）取得成功

--- scrapingA8_eucjp.js
+++ scrapingA8_eucjp.js
@@ -7,6 +7,7 @@ var targetUrl='http://www.a8.net/';
 var viewX = 1024; // viewportの設定：幅
 var viewY = 768;  // viewportの設定：高さ
 var flag = 0; // ページロード時の動作指定。
+var csvs = [];
 
 var casper = require('casper').create({
 	verbose: true,
@@ -14,6 +15,67 @@ var casper = require('casper').create({
 });
 
 casper.userAgent("Mozilla/5.0 (X11; Linux i686; rv:17.0) Gecko/20130402 Firefox/17.0"); // Linux使用してるんでこうなってます。適宜変更ねがいます
+
+function getLines(){
+	// １ぺーじ分のデータ取得
+	var tableObj = document.querySelector('table.programSearch:not(#element)').tBodies[0];
+	var csvLines=[];
+	for (var i=0; i< tableObj.rows.length; i++){
+		//console.log('loop : ' + i);
+		var oneLine = tableObj.rows[i];
+		csvLines[i]= (function(obj) {
+		var line = "csvline :"; 
+		line += obj.cells[0].innerHTML.replace(/\n/g, "").replace(/\t/g, "").replace(/\ +/g, " ").replace(/\"/g, "&dq;").replace(/\'/g, "&sq;");//行番号
+		line += " , ";
+		var detailTableObj = obj.cells[1].getElementsByTagName("table")[0]; //.querySelector('table');// 詳細テーブルを取得
+		var j=0;
+		
+			line += detailTableObj.rows[j++].cells[1].innerHTML.replace(/\n/g, "").replace(/\t/g, "").replace(/\ +/g, " ").replace(/\"/g, "&dq;").replace(/\'/g, "&sq;");//広告主
+			line += " , ";
+			line += detailTableObj.rows[j].cells[1].innerHTML.replace(/\n/g, "").replace(/\t/g, "").replace(/\ +/g, " ").replace(/\"/g, "&dq;").replace(/\'/g, "&sq;");// プログラム名
+			line += " , ";
+			line += /(s\d{14})/.exec(detailTableObj.rows[j++].cells[1].innerHTML.replace(/\n/g, "").replace(/\t/g, "").replace(/\ +/g, " ").replace(/\"/g, "&dq;").replace(/\'/g, "&sq;"))[0];// プログラムＩＤ
+			line += " , ";
+			line += detailTableObj.rows[j++].cells[1].innerHTML.replace(/\n/g, "").replace(/\t/g, "").replace(/\ +/g, " ").replace(/\"/g, "&dq;").replace(/\'/g, "&sq;");// 対応デバイス
+			line += " , ";
+			line += detailTableObj.rows[j++].cells[1].innerHTML.replace(/\n/g, "").replace(/\t/g, "").replace(/\ +/g, " ").replace(/\"/g, "&dq;").replace(/\'/g, "&sq;");// 成果報酬
+			line += " , ";
+			var imgObj = detailTableObj.rows[j].cells[0].getElementsByTagName('img')[0];
+			if (imgObj != null && /campaign\.gif/.test(imgObj.src) == true){
+				line += detailTableObj.rows[j++].cells[1].innerHTML.replace(/\n/g, "").replace(/\t/g, "").replace(/\ +/g, "").replace(/\"/g, "").replace(/\'/g, "&sq;");// キャンペーン中
+			}
+			line += ' , '; 
+			line += detailTableObj.rows[j].cells[1].innerHTML.replace(/\n/g, "").replace(/\t/g, "").replace(/\ +/g, " ").replace(/\"/g, "&dq;").replace(/\'/g, "&sq;");// 提携審査
+			line += " , ";
+			line += detailTableObj.rows[j].cells[3].innerHTML.replace(/\n/g, "").replace(/\t/g, "").replace(/\ +/g, " ").replace(/\"/g, "&dq;").replace(/\'/g, "&sq;");// 再訪問期間
+			//console.log('再訪問期間');
+			line += " , ";
+			line += detailTableObj.rows[j++].cells[5].innerHTML.replace(/\n/g, "").replace(/\t/g, "").replace(/\ +/g, " ").replace(/\"/g, "&dq;").replace(/\'/g, "&sq;");// 成果確定目安
+			line += " , ";
+			line += detailTableObj.rows[j++].cells[1].innerHTML.replace(/\n/g, "").replace(/\t/g, "").replace(/\ +/g, " ").replace(/\"/g, "&dq;").replace(/\'/g, "&sq;");// キーワード
+			line += " , ";
+			line += detailTableObj.rows[j++].cells[0].innerHTML.replace(/\n/g, "").replace(/\t/g, "").replace(/\ +/g, " ").replace(/\"/g, "&dq;").replace(/\'/g, "&sq;");// アイコンエリア１
+			line += " , ";
+		
+		line += obj.cells[2].innerHTML.replace(/\n/g, "").replace(/\t/g, "").replace(/\ +/g, " ").replace(/\"/g, "&dq;").replace(/\'/g, "&sq;");// 関連情報（アイコンエリア２）
+			line += " , ";
+		line += obj.cells[3].innerHTML.replace(/\n/g, "").replace(/\t/g, "").replace(/\ +/g, " ").replace(/\"/g, "&dq;").replace(/\'/g, "&sq;");// 提携対応（アイコンエリア３）
+			line += " , ";
+		line += obj.cells[4].innerHTML.replace(/\n/g, "").replace(/\t/g, "").replace(/\ +/g, " ").replace(/\"/g, "&dq;").replace(/\'/g, "&sq;");// 一括申し込み（ちぇっくぼっくす）
+		
+		//console.log(line)
+
+		return line;
+		})(oneLine);
+
+	}
+		//return Array.prototype.map.call(csvLines, function(e){
+		//		return e;
+		//	});
+			 
+		return csvLines;
+
+}
 
 
 
@@ -120,12 +182,8 @@ casper.on('A8searchresult.loaded', function(){
 			itemAllLast = parseInt(valuesArray[0]);
 			itemCurrentLast = parseInt(valuesArray[2]);
 			this.echo('total ='+ valuesArray[0]+' , ' + 'current till ' + valuesArray[2]);
-			this.then(function(){
-				this.capture('search_result'+String(valuesArray[2])+'.png', {
-					top: 2500, left: 0,
-					width: viewX, height: viewY*3
-				}); //  スクショ
-			});
+			csvs = this.evaluate(getLines);
+			this.echo(csvs.join('\n'));
 			this.then(function(){
 				this.emit('A8searchresult.nextpage');
 			});
@@ -147,3 +205,5 @@ casper.on('A8searchresult.nextpage', function(){
 
 
 casper.run();
+
+
